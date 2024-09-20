b4a11c6f0c7ad19d81cfef9878617ef6f9f74423
onefuncman <onefuncman@users.noreply.github.com>
2024-09-19 20:18:32 -0500

    add alignment and name lookups for monster, optimize in-memory usage

--- src/routines/monsters.js
+++ src/routines/monsters.js
@@ -1,23 +1,61 @@
-const path = require("path");
 const readJsonLinesFile = require("./dataImport");
+
+class Monster {
+  constructor(data) {
+    this.Align = data.Align;
+    this.Name = data.Name;
+    this.Id = data.Id;
+
+    // uncomment this to see all the data but dont leave it checked in to save memory
+    //Object.assign(this, data);
+  }
+
+  get alignment() {
+    switch (this.Align) {
+      case 0:
+        return "Good";
+      case 1:
+        return "Evil";
+      case 2:
+        return "Chaotic Evil";
+      case 3:
+        return "Neutral";
+      case 4:
+        return "Lawful Good";
+      case 5:
+        return "Neutral Evil";
+      case 6:
+        return "Lawful Evil";
+      default:
+        return "Unknown";
+    }
+  }
+}
 
 class MonsterLookup {
   constructor(filepath) {
     this.monsters = {};
+    this.nameToIdMap = {};
     this.loadMonsters(filepath);
   }
 
   loadMonsters(filepath) {
     const monsterData = readJsonLinesFile(filepath);
-    monsterData.forEach((monster) => {
-      this.monsters[monster.id] = monster;
+    monsterData.forEach((data) => {
+      const monster = new Monster(data);
+      this.monsters[data.id] = monster;
+      this.nameToIdMap[data.Name.toLowerCase()] = data.id;
     });
   }
 
-  getMonster(id) {
+  getMonsterById(id) {
     return this.monsters[id] || null;
+  }
+
+  getMonsterByName(name) {
+    const id = this.nameToIdMap[name.toLowerCase()];
+    return id ? this.monsters[id] : null;
   }
 }
 
-// Export the MonsterLookup class
-module.exports = MonsterLookup;
+module.exports = { Monster, MonsterLookup };
