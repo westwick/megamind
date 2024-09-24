758c43032ac3521daa182d84fe20d4b271a5282d
Andrew Westwick <awestwick@gmail.com>
2024-09-24 18:10:34 -0500

    move components from index.html to app.vue, update styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="text-white">
-    <ConversationsComponent />
+  <div id="main-container">
+    <div class="flex w-full">
+      <div id="terminal"></div>
+      <div class="text-white flex-grow">
+        <ConversationsComponent />
+      </div>
+    </div>
+    <input
+      type="text"
+      class="main-input w-full bg-gray-800 text-white border-gray-700 p-1 focus:outline-none focus:border-blue-500"
+      placeholder=""
+    />
+    <div id="debugger">
+      <pre>init</pre>
+    </div>
   </div>
 </template>
 
 <script setup>
 import ConversationsComponent from "./components/ConversationsComponent.vue";
 </script>
+
+<style>
+.main-input {
+  border: 1px solid #424242;
+}
+</style>
