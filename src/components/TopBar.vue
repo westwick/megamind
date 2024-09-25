27c9b21821c692a78ed8ebccd3de7b89f2822eb4
Andrew Westwick <awestwick@gmail.com>
2024-09-25 14:09:34 -0500

    add more icons to TopBar, add active state and some temporary control logic (this should instead be based on config in future)

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="relative">
     <div class="topbar">
-      <div class="flex items-center space-x-2">
-        <div class="icon-container" title="Connect to server">
-          <PlugZap class="icon" fill="yellow" />
+      <div class="flex items-center">
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': isConnectedToServer }"
+          title="Connect to server"
+        >
+          <PlugZap class="icon" :fill="isConnectedToServer ? 'yellow' : ''" />
         </div>
-        <div class="icon-container" title="Auto-Combat">
-          <Swords class="icon" fill="orange" />
+        <div class="separator"></div>
+        <div class="icon-container" title="Go to location">
+          <MoveUpRight class="icon" fill="" />
         </div>
-        <div class="icon-container" title="Auto-Heal/Rest">
-          <Cross class="icon" fill="red" />
+        <div class="icon-container" title="Loop an area">
+          <Repeat class="icon" fill="" />
+        </div>
+        <div class="icon-container" title="Step backwards">
+          <UndoDot class="icon" fill="" />
+        </div>
+        <div class="icon-container icon-active" title="Stop moving">
+          <Octagon class="icon" fill="rgba(100, 0, 0, 0.2)" />
+        </div>
+        <div class="separator"></div>
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': autoAll }"
+          title="Toggle all auto-actions"
+          @click="toggleAutoAll"
+        >
+          <Power class="icon" fill="rgba(0, 0, 0, 0)" />
+        </div>
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': autoCombat }"
+          title="Auto-Combat"
+          @click="toggleAutoAction('autoCombat')"
+        >
+          <Swords class="icon" :fill="autoCombat ? 'red' : ''" />
+        </div>
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': autoHeal }"
+          title="Auto-Heal/Rest"
+          @click="toggleAutoAction('autoHeal')"
+        >
+          <Cross class="icon" :fill="autoHeal ? 'green' : ''" />
+        </div>
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': autoBless }"
+          title="Auto-Bless"
+          @click="toggleAutoAction('autoBless')"
+        >
+          <Sparkles class="icon" :fill="autoBless ? 'blue' : ''" />
+        </div>
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': autoGet }"
+          title="Auto-Get"
+          @click="toggleAutoAction('autoGet')"
+        >
+          <Grab class="icon" fill="" />
+        </div>
+        <div
+          class="icon-container"
+          :class="{ 'icon-active': autoSneak }"
+          title="Auto-Sneak"
+          @click="toggleAutoAction('autoSneak')"
+        >
+          <Footprints class="icon" :fill="autoSneak ? 'purple' : ''" />
         </div>
       </div>
-      <div class="flex items-center space-x-2">
+      <div class="flex items-center">
         <div
           class="icon-container"
           :class="{ active: isSettingsOpen }"
@@ -33,36 +93,101 @@
 
 <script setup>
 import { ref } from "vue";
-import { PlugZap, Swords, Cross, User } from "lucide-vue-next";
+import {
+  PlugZap,
+  MoveUpRight,
+  Repeat,
+  UndoDot,
+  Octagon,
+  Power,
+  Swords,
+  Cross,
+  Sparkles,
+  Grab,
+  Footprints,
+  User,
+} from "lucide-vue-next";
 
 const isSettingsOpen = ref(false);
+const isConnectedToServer = ref(true);
+const isStopMoving = ref(true);
+
+const autoAll = ref(true);
+const autoCombat = ref(true);
+const autoHeal = ref(true);
+const autoBless = ref(true);
+const autoGet = ref(true);
+const autoSneak = ref(true);
 
 const toggleSettings = () => {
   isSettingsOpen.value = !isSettingsOpen.value;
+};
+
+const toggleAutoAll = () => {
+  autoAll.value = !autoAll.value;
+  if (autoAll.value) {
+    autoCombat.value = true;
+    autoHeal.value = true;
+    autoBless.value = true;
+    autoGet.value = true;
+    autoSneak.value = true;
+  }
+};
+
+const toggleAutoAction = (actionName) => {
+  switch (actionName) {
+    case "autoCombat":
+      autoCombat.value = !autoCombat.value;
+      break;
+    case "autoHeal":
+      autoHeal.value = !autoHeal.value;
+      break;
+    case "autoBless":
+      autoBless.value = !autoBless.value;
+      break;
+    case "autoGet":
+      autoGet.value = !autoGet.value;
+      break;
+    case "autoSneak":
+      autoSneak.value = !autoSneak.value;
+      break;
+  }
 };
 </script>
 
 <style scoped>
 .topbar {
-  @apply w-full h-10 bg-zinc-900 border-b border-gray-800 flex justify-between items-center px-1 text-gray-400;
+  @apply w-full bg-zinc-900 border-b border-gray-700 flex justify-between items-stretch px-1 text-gray-400;
   position: relative;
   z-index: 10;
 }
 
 .icon-container {
-  @apply p-1 rounded-sm border border-gray-700 hover:border-gray-500 hover:text-gray-200 cursor-pointer;
+  @apply p-1 m-1 rounded-sm border border-gray-500 hover:border-blue-400 hover:text-gray-200 cursor-pointer;
+}
+
+.icon-active {
+  @apply border-blue-500;
+  background-color: rgba(212, 243, 255, 0.1);
+}
+
+.icon-active .icon {
+  @apply text-gray-100;
 }
 
 .icon {
   @apply w-5 h-5;
 }
 
-.icon-container.active {
-  @apply border-gray-500 text-gray-200 bg-gray-800;
+.separator {
+  @apply w-px mx-1 bg-gray-700 self-stretch;
 }
 
 .settings-area {
-  @apply w-full bg-zinc-800 border-b border-gray-700;
+  @apply w-full border-b border-gray-700;
+  /*bg-zinc-800*/
+  background-color: rgba(55, 65, 81, 0.9);
+  backdrop-filter: blur(2px);
   position: absolute;
   top: 100%;
   left: 0;
@@ -73,7 +198,7 @@ const toggleSettings = () => {
 
 .slide-enter-active,
 .slide-leave-active {
-  transition: all 0.3s ease-out;
+  transition: all 0.2s ease-in-out;
 }
 
 .slide-enter-from,
--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ let mainWindow;
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 1600,
+    height: 1200,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
